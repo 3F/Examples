df8fc93c9079d17787eaeddca62909d35b95ef3f
Denis Kuzmin [ GitHub/3F ] <x-3F@outlook.com>
2020-02-02 02:56:36 +0300

    +.NET Core and DllExport 1.7

---
+++ DllExport/BasicExport/DotNetCoreLibrary3/Alg.cs
@@ -0,0 +1,25 @@
+﻿// This code is part of example that was prepared specially for https://github.com/3F/Examples
+// Copyright (c) 2018,2020  Denis Kuzmin < x-3F@outlook.com > GitHub/3F
+// Distributed under the MIT License
+
+using System;
+
+namespace DotNetCoreLibrary3
+{
+    public class Alg
+    {
+        /// <summary>
+        /// Amazing thing through cos ...
+        /// cosine with a shift.
+        /// </summary>
+        /// <param name="ofs"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>possible #IND (indeterminate) –INF / +INF</returns>
+        [DllExport]
+        public static double AmazingCos(int ofs, double x, double y)
+        {
+            return y / 2 * Math.Cos(ofs * 2 * Math.PI / x);
+        }
+    }
+}
